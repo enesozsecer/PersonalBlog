bba89365fca06c844653d4048ebba91602150d81
Enes <89150347+enesozsecer@users.noreply.github.com>
2024-04-05 16:59:39 +0300

    Değişiklikler kaydedildi.

--- MyBlog.Entity/Entity/Work.cs
+++ MyBlog.Entity/Entity/Work.cs
@@ -15,6 +15,6 @@ namespace MyBlog.Entity.Entity
         public string Description { get; set; }
         public string Photo { get; set; }
         public string Url { get; set; }
-        public bool CurrentlyJob { get; set; } = false;
+        public bool CurrentlyJob { get; set; }
     }
 }
